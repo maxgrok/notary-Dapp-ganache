443bd9199a2c8cebc6167749c00825a8b654c0fc
Max Goodman <max@maxgrok.com>
2020-01-31 10:40:58 -0500

    adding edits to netlify/build

---
+++ .netlify/build/precache-manifest.484d326b375e4fc7e20c4494dd074cd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98966469bf306ae58947c67a6d6ba23d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bba9843a857c2aadb99",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "eab8f592b62cc850c983",
+    "url": "/static/js/2.9dd0ca19.chunk.js"
+  },
+  {
+    "revision": "cc7fe55eacfaba8d53c98419b0ca6558",
+    "url": "/static/js/2.9dd0ca19.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8bba9843a857c2aadb99",
+    "url": "/static/js/main.d7aa3c9b.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
